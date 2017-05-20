aec8b63525f5a020f7c85e3327c15ee67706836a
RomanKomac <rk3367@student.uni-lj.si>
2017-05-20 19:16:41 +0200

    neki7

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-app.listen(server_port, function () {
-  console.log('Example app listening on port ' + server_port);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
